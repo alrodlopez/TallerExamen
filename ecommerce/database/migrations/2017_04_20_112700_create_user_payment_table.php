7dc1b568c2bf2264b2d1f493a6a10e6f6222a3ae
Juanqca <jctmtorrez666@gmail.com>
2017-04-20 10:15:51 -0400

    magracion 7

---
+++ ecommerce/database/migrations/2017_04_20_112700_create_user_payment_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserPaymentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('user_payment',function (Blueprint $tabla){
+            $tabla->increments("id");
+            $tabla->integer("user_id")->unsigned();
+            $tabla->foreign("user_id")->references("id")->on("users");
+            $tabla->integer("payment_type_id")->unsigned();
+            $tabla->foreign("payment_type_id")->references("idPayType")->on("payment_types");
+            $tabla->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+
+    }
+}
