a33705bdf2f3a648395123c3d727db3293fd075a
Juanqca <jctmtorrez666@gmail.com>
2017-04-20 14:57:19 -0400

    migration12

---
+++ ecommerce/database/migrations/2017_04_20_185102_create_post_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('post_products',function (Blueprint $tabla){
+            $tabla->increments("id");
+            $tabla->decimal("price",9,2);
+            $tabla->integer("post_id")->unsigned();
+            $tabla->foreign("post_id")->references("id")->on("posts");
+            $tabla->integer("product_id")->unsigned();
+            $tabla->foreign("product_id")->references("id")->on("products");
+            $tabla->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('post_products');
+    }
+}
