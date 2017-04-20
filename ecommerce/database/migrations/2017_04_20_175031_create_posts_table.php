d72d0affcd4ba843f26654957fd809b1a65583c8
Juanqca <jctmtorrez666@gmail.com>
2017-04-20 13:57:28 -0400

    tabla posts

---
+++ ecommerce/database/migrations/2017_04_20_175031_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('posts',function (Blueprint $tabla){
+            $tabla->increments("id");
+            $tabla->integer("user_id")->unsigned();
+            $tabla->foreign("user_id")->references("id")->on("users");
+            $tabla->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('posts');
+    }
+}
