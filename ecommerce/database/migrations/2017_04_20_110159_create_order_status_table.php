806cd11cbbf524e58f82ebde829ee8f4b176b16a
alrodlopez <alrodlopez@gmail.com>
2017-04-20 07:07:35 -0400

    Migrate5

---
+++ ecommerce/database/migrations/2017_04_20_110159_create_order_status_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderStatusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('order_status',function (Blueprint $tabla){
+            $tabla->increments("idStatus");
+            $tabla->string('Description');
+            $tabla->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('order_status');
+    }
+}
