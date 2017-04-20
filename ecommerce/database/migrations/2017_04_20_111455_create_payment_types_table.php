bd59f150b8be34ee714a0169e5ef1ded1890fc4f
alrodlopez <alrodlopez@gmail.com>
2017-04-20 07:20:05 -0400

    Migrate6

---
+++ ecommerce/database/migrations/2017_04_20_111455_create_payment_types_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('payment_types',function (Blueprint $tabla){
+            $tabla->increments("idPayType");
+            $tabla->string('Description');
+            $tabla->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('payment_types');
+    }
+}
