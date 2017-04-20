4850f3b300d3059047566e332191bca31a2889e3
Juanqca <jctmtorrez666@gmail.com>
2017-04-20 15:20:49 -0400

    migration14

---
+++ ecommerce/database/migrations/2017_04_20_191742_create_product_promotions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductPromotionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('product_promotions',function (Blueprint $tabla){
+            $tabla->increments("id");
+            $tabla->integer("promotion_id")->unsigned();
+            $tabla->foreign("promotion_id")->references("id")->on("promotions");
+            $tabla->integer("product_id")->unsigned();
+            $tabla->foreign("product_id")->references("id")->on("products");
+            $tabla->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('product_promotions');
+    }
+}
