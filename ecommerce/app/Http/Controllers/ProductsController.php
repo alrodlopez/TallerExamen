780933a55e0e5112e5d8781b2912f0188b295e82
Juanqca <jctmtorrez666@gmail.com>
2017-04-27 21:54:31 -0400

    Controlador de productos

--- ecommerce/app/Http/Controllers/ProductsController.php
+++ ecommerce/app/Http/Controllers/ProductsController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Products;
 use Illuminate\Http\Request;
+use DB;
 
 class ProductsController extends Controller
 {
     //
+    public function listAll()
+    {
+        $products = Products::All();
+        return view('products.listAll',['products'=> $products]);
+    }
+    public function create()
+    {
+
+    }
+    public function update()
+    {
+
+    }
+    public function delete()
+    {
+
+    }
+    public function show()
+    {
+
+    }
 }
