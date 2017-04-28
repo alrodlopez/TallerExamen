52d5e650c431facf48ea0ab4e3c4941533e7b431
alrodlopez <alrodlopez@gmail.com>
2017-04-28 01:04:31 -0400

    Productos view

--- ecommerce/resources/views/seccion_productos.blade.php
+++
@@ -1,5 +0,0 @@
-@extends ('layouts.app')
-@section ('title','Seccion productos')
-    @section('content')
-        <h1> Productos </h1>
-        @endsection
--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -21,18 +21,14 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/', 'seccion_productosController@mostrar');
-
 
 
 //PRODUCTS CRUD
 Route::get('products','ProductsController@listall');
-Route::get('products\create','ProductsController@create');
-Route::get('products\update','ProductsController@update');
-Route::get('products\delete','ProductsController@delete');
-Route::get('products\show','ProductsController@show');
+//Route::get('products\create','ProductsController@create');
+//Route::get('products\update','ProductsController@update');
+//Route::get('products\delete','ProductsController@delete');
+//Route::get('products\show','ProductsController@show');
 
