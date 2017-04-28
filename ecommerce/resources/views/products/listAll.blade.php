6cef7a9498e00067aa5a2dd8ef7137739ab07cd6
Juanqca <jctmtorrez666@gmail.com>
2017-04-27 21:58:59 -0400

    Vista de los productos para el vendedor

---
+++ ecommerce/resources/views/products/listAll.blade.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+<h1>Listado de Productos</h1>
+
+<table>
+    <tr>
+        <th>ID</th>
+        <th>NOMBRE</th>
+        <th>DESCRIPCION</th>
+        <th>Tipo</th>
+        <th>Proveedor</th>
+    </tr>
+    @foreach($products as $product)
+        <tr>
+        <td>{{$product->id}}</td>
+        <td>{{$product->name}}</td>
+        <td>{{$product->description}}</td>
+        <td>{{$product->product_type_id}}</td>
+        <td>{{$product->provider_id}}</td>
+        </tr>
+    @endforeach
+
+</table>
+</body>
+</html>
