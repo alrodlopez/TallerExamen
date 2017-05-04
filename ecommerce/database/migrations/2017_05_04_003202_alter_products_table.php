b1349c0a188393d9aa3cb635c25e116620e77d7a
Juanqca <jctmtorrez666@gmail.com>
2017-05-04 00:10:13 -0400

    MOdicficacion base de datos

---
+++ ecommerce/database/migrations/2017_05_04_003202_alter_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function($tabla){
+            $tabla->increments("id");
+            $tabla->string("name");
+            $tabla->text("description");
+            $tabla->decimal("pricing",9,2);
+            $tabla->integer("product_type_id")->unsigned();
+            $tabla->foreign("product_type_id")->references("id")->on("product_types");
+            $tabla->integer("provider_id")->unsigned();
+            $tabla->foreign("provider_id")->references("id")->on("providers");
+            $tabla->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
