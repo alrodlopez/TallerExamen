e73fa0d1e763f2207ebf145e99add14ab44bc3a4
Juanqca <jctmtorrez666@gmail.com>
2017-05-05 05:11:02 -0400

    estilos de interfaz

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -18,6 +18,7 @@
 Route::get('/','MainController@home');
 
 Auth::routes();
+Route::resource('products','ProductsController');
 
 Route::get('/home', 'HomeController@index');
 
@@ -27,7 +28,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 //PRODUCTS CRUD
 Route::get('products','ProductsController@listAll');
-Route::get('products\create','ProductsController@create');
+Route::post('products\create','ProductsController@create');
 Route::get('products\update','ProductsController@update');
 Route::get('products\delete','ProductsController@delete');
 Route::get('products\show','ProductsController@show');
