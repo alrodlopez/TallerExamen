631a74c296568d0c1d775f62b6fe9dbe2439d1d3
suvargas <suvargasportugal@gmail.com>
2017-05-06 01:45:38 -0400

    interface panel de control

--- ecommerce/resources/views/panelcontrol/pc.blade.php
+++ ecommerce/resources/views/panelcontrol/pc.blade.php
@@ -1,21 +1,25 @@
-@extends('layouts.app');
+@extends('layouts.app')
 @section('content')
     <div class="big-padding text-center blue-grey white-text">
-        <h1>Usuario Vendedor Datos</h1>
+        <h1>Panel Control usuarios</h1>
     </div>
     <div class="container">
         <table class="table table-bordered">
             <thead>
-            <tr><td>ID</td></tr>
-            <tr><td>Name</td></tr>
-            <tr><td>Last Name</td></tr>
-            <tr><td>Phone</td></tr>
-            <tr><td>EMail</td></tr>
-            <tr><td>Address</td></tr>
-            </thead>
-            <tbody>
+           <tr><td><a href="{{url('users')}}" class="btn btn-primary btn-fab ">Datos Personales<i class="material-icons" ></i></a></td></tr>
 
-            </tbody>
+            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-primary btn-fab ">Datos Financieros<i class="material-icons" ></i></a></td></tr>
+            <tr><td><a href="{{url('/products/create')}}" class="btn btn-primary btn-fab ">Ranking y reputacion<i class="material-icons" ></i></a></td></tr>
+            <tr><td><a href="{{url('/products/create')}}" class="btn btn-primary btn-fab ">Mensajes<i class="material-icons" ></i></a></td></tr>
+            <tr><td><a href="{{url('/products/create')}}" class="btn btn-primary btn-fab ">Productos y servicios<i class="material-icons" ></i></a></td></tr>
+            <tr><td><a href="{{url('/products/create')}}" class="btn btn-primary btn-fab ">Seguridad y contraseñas<i class="material-icons" ></i></a></td></tr>
+
+            </thead>
+
         </table>
+    </div>
+    <div align="center" class="floating">
+
+        </a>
     </div>
 @endsection
