48e4423c720a8307088b108f865d1ffa0ca81cdd
suvargas <suvargasportugal@gmail.com>
2017-05-05 23:58:48 -0400

    agrgado controlador para panel de control

---
+++ ecommerce/resources/views/panelcontrol/pc.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app');
+@section('content')
+    <div class="big-padding text-center blue-grey white-text">
+        <h1>Usuario Vendedor Datos</h1>
+    </div>
+    <div class="container">
+        <table class="table table-bordered">
+            <thead>
+            <tr><td>ID</td></tr>
+            <tr><td>Name</td></tr>
+            <tr><td>Last Name</td></tr>
+            <tr><td>Phone</td></tr>
+            <tr><td>EMail</td></tr>
+            <tr><td>Address</td></tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+@endsection
