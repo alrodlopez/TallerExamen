0c16a7970a3b8721a2328db089f308d176dc1be6
suvargas <suvargasportugal@gmail.com>
2017-05-06 01:46:31 -0400

    interface panel de control

--- ecommerce/resources/views/panelcontrol/pc.blade.php
+++ ecommerce/resources/views/panelcontrol/pc.blade.php
@@ -9,10 +9,10 @@
            <tr><td><a href="{{url('users')}}" class="btn btn-primary btn-fab ">Datos Personales<i class="material-icons" ></i></a></td></tr>
 
             <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-primary btn-fab ">Datos Financieros<i class="material-icons" ></i></a></td></tr>
-            <tr><td><a href="{{url('/products/create')}}" class="btn btn-primary btn-fab ">Ranking y reputacion<i class="material-icons" ></i></a></td></tr>
-            <tr><td><a href="{{url('/products/create')}}" class="btn btn-primary btn-fab ">Mensajes<i class="material-icons" ></i></a></td></tr>
-            <tr><td><a href="{{url('/products/create')}}" class="btn btn-primary btn-fab ">Productos y servicios<i class="material-icons" ></i></a></td></tr>
-            <tr><td><a href="{{url('/products/create')}}" class="btn btn-primary btn-fab ">Seguridad y contraseñas<i class="material-icons" ></i></a></td></tr>
+            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-primary btn-fab ">Ranking y reputacion<i class="material-icons" ></i></a></td></tr>
+            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-primary btn-fab ">Mensajes<i class="material-icons" ></i></a></td></tr>
+            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-primary btn-fab ">Productos y servicios<i class="material-icons" ></i></a></td></tr>
+            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-primary btn-fab ">Seguridad y contraseñas<i class="material-icons" ></i></a></td></tr>
 
             </thead>
 
