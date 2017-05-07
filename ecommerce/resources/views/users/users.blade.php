b42fe1233b613b29859bdbe43aab199efed24bf3
suvargas <suvargasportugal@gmail.com>
2017-05-07 18:37:37 +0000

    Panel de control Usuarios datos funcionales

--- ecommerce/resources/views/users/users.blade.php
+++ ecommerce/resources/views/users/users.blade.php
@@ -6,17 +6,19 @@
     <div class="container">
         <table class="table table-bordered">
             <thead>
+            @foreach($users as $user)
             <h1>Datos</h1>
-            <tr><td>ID</td></tr>
-            <tr><td>Name</td><td><a class="btn btn-primary btn-fab ">Modificar<i class="material-icons" ></td></tr>
-            <tr><td>Last Name</td><td><a class="btn btn-primary btn-fab ">Modificar<i class="material-icons" ></td></tr>
-            <tr><td>Phone</td><td><a class="btn btn-primary btn-fab ">Modificar<i class="material-icons" ></td></tr>
-            <tr><td>EMail</td><td><a class="btn btn-primary btn-fab ">Modificar<i class="material-icons" ></td></tr>
-            <tr><td>Address</td><td><a class="btn btn-primary btn-fab ">Modificar<i class="material-icons" ></td></tr>
+            <tr><td>ID</td><td>{{$user->id}}</td></tr>
+            <tr><td>Name</td><td>{{$user->name}}</td></tr>
+            <tr><td>Email</td><td>{{$user->email}}</td></tr>
+            <tr><td>Password</td><td>{{$user->password}}</td></tr>
+            @endforeach
             </thead>
-            <tbody>
-
-            </tbody>
         </table>
+    </div>
+    <div align="center" class="floating">
+        <a href="/users/edit" class="btn btn-primary btn-fab">
+            Modificar Datos
+        </a>
     </div>
 @endsection
