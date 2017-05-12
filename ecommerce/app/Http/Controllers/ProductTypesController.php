0ee5991f797d0af0d9dafc46010c8a1462eee1e0
Juanqca <jctmtorrez666@gmail.com>
2017-05-12 06:35:09 -0400

    controlador tipoproduct

--- ecommerce/app/Http/Controllers/ProductTypesController.php
+++ ecommerce/app/Http/Controllers/ProductTypesController.php
@@ -3,8 +3,108 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\ProductTypes;
+use Illuminate\Support\Facades\Auth;
 
 class ProductTypesController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $product_type = ProductTypes::all();
+        return view("product_types.listado", ["product_types" => $product_type]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        $product_type = new ProductTypes;
+        return view("product_types.create",["product_types"=>$product_type]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $product_type = new ProductTypes;
+
+        $product_type->description =$request->description;
+        if($product_type->save()){
+            return redirect("/product_types");
+        }else{
+            return view("product_types.create");
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $product_type = ProductTypes::find($id);
+        return view("product_types.edit",["product_types"=>$product_type]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $product_type = ProductTypes::find($id);
+        $product_type->description =$request->description;
+        if($product_type->save()){
+            return redirect("/product_types");
+        }else{
+            //return view("product_types.edit");
+            return view ("product_types/create",["product_types"=>$product_type]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        ProductTypes::destroy($id);
+        return redirect ('/product_types');
+    }
 }
