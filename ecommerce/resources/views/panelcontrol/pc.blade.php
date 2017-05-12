922ee7a10e1052245f5303f89099248be1df872f
suvargas <suvargasportugal@gmail.com>
2017-05-12 03:19:48 +0000

    adaptando a stilos

--- ecommerce/resources/views/panelcontrol/pc.blade.php
+++ ecommerce/resources/views/panelcontrol/pc.blade.php
@@ -6,13 +6,13 @@
     <div class="container">
         <table class="table table-bordered">
             <thead>
-           <tr><td><a href="{{url('users')}}" class="btn btn-success">Datos Personales<i class="material-icons" ></i></a></td></tr>
+           <tr><td><a href="{{url('users')}}" class="btn btn-success">Datos Personales<i class="material-icons" ></i></a> Ver y administrar los datos personales del vendedor y establecer o editar el acceso</td></tr>
 
-            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-success">Datos Financieros<i class="material-icons" ></i></a></td></tr>
-            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-success">Ranking y reputacion<i class="material-icons" ></i></a></td></tr>
-            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-success">Mensajes<i class="material-icons" ></i></a></td></tr>
-            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-success">Productos y servicios<i class="material-icons" ></i></a></td></tr>
-            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-success">Seguridad y contraseñas<i class="material-icons" ></i></a></td></tr>
+            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-success">Datos Financieros<i class="material-icons" ></i></a> Ver y administrar los datos financieros del vendedor </td></tr>
+            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-success">Ranking y reputacion<i class="material-icons" ></i></a> Establece un status del vendedor para mayor confiabilidad</td></tr>
+            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-success">Mensajes<i class="material-icons" ></i></a> Ver y administrar los mensajes de clientes u otros vendedores</td></tr>
+            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-success">Productos y servicios<i class="material-icons" ></i></a> Ver y administrar los productos y servicios ofrecidos por el vendedor</td></tr>
+            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-success">Seguridad y contraseñas<i class="material-icons" ></i></a> Ver y administrar la seguridad de datos de acceso del vendedor</td></tr>
 
             </thead>
 
