791b889e2ac3353cea7447a354ea532dbcffb409
Juanqca <jctmtorrez666@gmail.com>
2017-05-17 21:05:34 -0400

    Agregando etiquetas al formulario de productos

--- ecommerce/resources/views/products/form.blade.php
+++ ecommerce/resources/views/products/form.blade.php
@@ -1,16 +1,41 @@
 {!! Form::open(['url'=> $url, 'method'=>$method]) !!}
+<thead>
+            <tr>
+                <td><b>NOMBRE:</b></td>
+            </tr>
+            </thead>
 <div class="form-group">
     {{Form::text('name',$product->name,['class'=>'form-control','placeholder'=> 'Nombre'])}}
 </div>
+<thead>
+            <tr>
+                <td><b>DESCRIPCIÓN:</b></td>
+            </tr>
+            </thead>
 <div class="form-group">
     {{Form::textarea('description',$product->description,['class'=>'form-control','placeholder'=>'Escribe tu descripcion'])}}
 </div>
+<thead>
+            <tr>
+                <td><b>PRECIO:</b></td>
+            </tr>
+            </thead>
 <div class="form-group">
     {{Form::number('pricing',$product->pricing,['class'=>'form-control','placeholder'=> 'Precio de tu producto'])}}
 </div>
+<thead>
+            <tr>
+                <td><b>TIPO DE PRODUCTO:</b></td>
+            </tr>
+            </thead>
 <div class="form-group">
     {{Form::number('product_type_id',$product->product_type_id,['class'=>'form-control','placeholder'=> 'Tipo de producto'])}}
 </div>
+<thead>
+            <tr>
+                <td><b>PROVEEDOR:</b></td>
+            </tr>
+            </thead>
 <div class="form-group">
     {{Form::number('provider_id',$product->provider_id,['class'=>'form-control','placeholder'=> 'Proveedor'])}}
 </div>
