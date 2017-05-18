e5481458a3cb34167e2d8841fe9cdf8d37620ab8
Juanqca <jctmtorrez666@gmail.com>
2017-05-17 21:03:12 -0400

    Modificando la vista de crear productos

--- ecommerce/resources/views/products/create.blade.php
+++ ecommerce/resources/views/products/create.blade.php
@@ -1,7 +1,7 @@
 @extends("layouts.app");
 @section("content")
     <div class="container white"></div>
-    <h1>Nuevo producto</h1>
+    <center><h1>Nuevo Producto</h1></center>
     @include('products.form',['product'=>$product, 'url'=>'/products', 'method'=>'POST'])
 
 @endsection
