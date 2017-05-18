349d07d6a2429af296f2f22d27bf444c6ba1f137
Juanqca <jctmtorrez666@gmail.com>
2017-05-17 20:13:29 -0400

    modificacion del edit

--- ecommerce/resources/views/product_types/edit.blade.php
+++ ecommerce/resources/views/product_types/edit.blade.php
@@ -2,6 +2,6 @@
 @section("content")
     <div class="container white">
         <h1>Editar Tipo de Producto</h1>
-        @include('product_types.form',['product_types'=>$product_type, 'url'=>'product_types'.$product_type->id, 'method'=>'PATCH'])
+        @include('product_types.form',['product_types'=>$product_type, 'url'=>'product_types/'.$product_type->id, 'method'=>'PATCH'])
     </div>
 @endsection
