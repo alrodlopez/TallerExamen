2bc5c9920b744bb39dd65e97ea85e4b697a707bf
suvargas <suvargasportugal@gmail.com>
2017-05-26 06:02:00 +0000

    corregidos rutas

--- ecommerce/resources/views/panelcontrol/pc.blade.php
+++ ecommerce/resources/views/panelcontrol/pc.blade.php
@@ -12,7 +12,7 @@
             <tr><td><a href="{{url('providers')}}" class="btn btn-success">Proveedores<i class="material-icons" ></i></a> Establece un status del vendedor para mayor confiabilidad</td></tr>
             <tr><td><a href="{{url('promotions')}}" class="btn btn-success">Promociones<i class="material-icons" ></i></a> Ver y administrar las promociones de clientes u otros vendedores</td></tr>
             <tr><td><a href="{{url('products')}}" class="btn btn-success">Productos<i class="material-icons" ></i></a> Ver y administrar los productos y servicios ofrecidos por el vendedor</td></tr>
-            <tr><td><a href="{{url('panelcontrol')}}" class="btn btn-success">Seguridad y contraseñas<i class="material-icons" ></i></a> Ver y administrar la seguridad de datos de acceso del vendedor</td></tr>
+            <tr><td><a href="{{url('seguridad')}}" class="btn btn-success">Seguridad y contraseñas<i class="material-icons" ></i></a> Ver y administrar la seguridad de datos de acceso del vendedor</td></tr>
 
             </thead>
 
